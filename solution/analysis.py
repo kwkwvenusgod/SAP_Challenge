d7cbb9039377119a43975d2e2cc5d64c48fc6851
kwkwvenusgod <kwkwvenusgod@gmail.com>
2018-01-15 11:27:27 +0800

    increase epochs

--- solution/analysis.py
+++ solution/analysis.py
@@ -1,4 +1,15 @@
-from Novel_CNN import NovelCnn
+import numpy as np
 
-model_path = 'myNovelCNN.pickle'
-ncnn = NovelCnn().load_ncnn_model(model_path)
+
+rest_train_file_path = 'rest_train.txt'
+rest_train = np.loadtxt(rest_train_file_path)
+ytrain_pred = rest_train[:,0:12]
+ytrain_true = rest_train[:,12:-1]
+
+ytrain_pred = ytrain_pred.argmax(axis=1)
+ytrain_true = ytrain_true.argmax(axis=1)
+
+
+
+
+
